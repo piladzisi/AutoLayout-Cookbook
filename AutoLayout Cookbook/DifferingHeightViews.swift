1cd9dacce6b3ed6ed66bdae379026fa76f629605
Anna <piladzisi@gmail.com>
2019-04-09 13:25:15 +0300

    proportional height in Code

---
+++ AutoLayout Cookbook/DifferingHeightViews.swift
@@ -0,0 +1,23 @@
+//
+//  DifferingHeightViews.swift
+//  AutoLayout Cookbook
+//
+//  Created by anna.sibirtseva on 09/04/2019.
+//  Copyright © 2019 anna.sibirtseva. All rights reserved.
+//
+
+import UIKit
+
+class DifferingHeightViewController: UIViewController {
+    let redView = UIView()
+    let greenView = UIView()
+    let blueView = UIView()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        redView.backgroundColor = .red
+        greenView.backgroundColor = .green
+        blueView.backgroundColor = .blue
+    }
+}

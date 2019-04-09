7e66800386ac4541d37915595f004cbfdf78037c
Anna <piladzisi@gmail.com>
2019-04-09 12:54:26 +0300

    equal width in Code done (before and after iOS 11.0

--- AutoLayout Cookbook/EqualWidthViews.swift
+++ AutoLayout Cookbook/EqualWidthViews.swift
@@ -30,7 +30,39 @@ class EqualWidthViewController: UIViewController {
         
         redView.translatesAutoresizingMaskIntoConstraints = false
         blueView.translatesAutoresizingMaskIntoConstraints = false
+        
+        activateLayoutConstraints()
+        
     }
+    func activateLayoutConstraints(){
+        if #available(iOS 11, *) {
+            let guide = view.safeAreaLayoutGuide
+            
+            NSLayoutConstraint.activate([
+                redView.leftAnchor.constraint(equalTo: guide.leftAnchor, constant: 8.0),
+                redView.topAnchor.constraint(equalTo: guide.topAnchor, constant: 8.0),
+                redView.bottomAnchor.constraint(equalTo: guide.bottomAnchor, constant: -8.0),
+                blueView.topAnchor.constraint(equalTo: guide.topAnchor, constant: 8.0),
+                blueView.bottomAnchor.constraint(equalTo: guide.bottomAnchor, constant: -8.0),
+                blueView.rightAnchor.constraint(equalTo: guide.rightAnchor, constant: -8.0)
+                ])
+        } else {
+            NSLayoutConstraint.activate([
+                redView.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 8.0),
+                redView.topAnchor.constraint(equalTo: topLayoutGuide.bottomAnchor, constant: 8.0),
+                redView.bottomAnchor.constraint(equalTo: bottomLayoutGuide.topAnchor, constant: -8.0),
+                blueView.topAnchor.constraint(equalTo: topLayoutGuide.bottomAnchor, constant: 8.0),
+                blueView.bottomAnchor.constraint(equalTo: bottomLayoutGuide.topAnchor, constant: -8.0),
+                blueView.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -8.0)
+            ])
+        }
+        
+        NSLayoutConstraint.activate([
+            redView.rightAnchor.constraint(equalTo: blueView.leftAnchor, constant: -8.0),
+            redView.widthAnchor.constraint(equalTo: blueView.widthAnchor, multiplier: 1.0)
+            ])
+    }
+    
 }
 
 
